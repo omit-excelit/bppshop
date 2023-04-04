b951b60efa404408bf44088fc5a9f34a8677e2e1
“maksudur-excelitai” <maksudur@excelitai.com>
2023-04-04 14:34:48 +0600

    Update

--- lib/pages/pending_commission_page.dart
+++ lib/pages/pending_commission_page.dart
@@ -1,4 +1,8 @@
+import 'package:bppshop/const/color.dart';
+import 'package:bppshop/const/style.dart';
+import 'package:bppshop/drawer/my_drawer.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class PendingCommissionPage extends StatefulWidget {
   const PendingCommissionPage({Key? key}) : super(key: key);
@@ -8,8 +12,24 @@ class PendingCommissionPage extends StatefulWidget {
 }
 
 class _PendingCommissionPageState extends State<PendingCommissionPage> {
+  final GlobalKey<ScaffoldState> _scaffoldkey = GlobalKey();
+  TextEditingController searchController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      drawer: MyDrawerPage(),
+      key: _scaffoldkey,
+      backgroundColor: bgColor,
+      appBar: AppBar(
+        backgroundColor: appBarColor,
+        centerTitle: false,
+        leading: InkWell(
+            onTap: (){
+              _scaffoldkey.currentState!.openDrawer();
+            },
+            child: Icon(Icons.menu, size: 16.5.sp, color: secondaryWhite,)),
+        title: Text("My Commission", style: myStyleMontserrat(18.sp, secondaryWhite, FontWeight.w400),),
+      ),
+    );
   }
 }
