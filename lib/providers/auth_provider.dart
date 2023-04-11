d9c5d994e0243f64ed30a215387c888427cf5f9d
omit-excelitai <omit2023@excelitai.com>
2023-04-11 10:04:39 +0600

    Update

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -1,5 +1,29 @@
 import 'package:flutter/material.dart';
 
 class AuthProvider with ChangeNotifier{
+  String _number = "";
+  String _password = "";
+  String _token = "";
+
+  String get getNumber => _number;
+  String get getPassword => _password;
+  String get getToken => _token;
+
+  set setToken(String value) {
+    _token = value;
+    notifyListeners();
+  }
+
+  set setNumber(String number){
+    _number = number;
+    notifyListeners();
+  }
+
+  set setPassword(String password){
+    _password = password;
+    notifyListeners();
+  }
+
+
 
 }
