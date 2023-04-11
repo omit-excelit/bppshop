437442dafdb19fb48f36f78462521243daf26250
omit-excelitai <omit2023@excelitai.com>
2023-04-11 15:49:30 +0600

    Update

--- lib/providers/district_provider.dart
+++ lib/providers/district_provider.dart
@@ -1,5 +1,11 @@
+import 'package:bppshop/model/district_model.dart';
 import 'package:flutter/material.dart';
 
 class DistrictProvider with ChangeNotifier{
+  List<DistrictModel> districtData = [];
 
+  getDistrictData() async {
+    districtData = await OrderHistoryHttpRequest.fetchOrderHistoryData();
+    notifyListeners();
+  }
 }
